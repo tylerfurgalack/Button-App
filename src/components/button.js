f194c8ff6c50ef863fd9573657b8079d6a2757e1
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-05-07 12:28:13 -0400

    hook added

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 
-const Button = (props) => {
-    return (
-        <button>Click Me</button>);
+const Button = () => {
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
 };
 
 export default Button;
