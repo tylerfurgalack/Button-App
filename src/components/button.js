a1b59a3c4d6a78fe2d5facbf03947569845ce863
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-05-07 12:40:38 -0400

    h1 count added

--- src/components/button.js
+++ src/components/button.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const Button = () => {
   const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+  return <>
+  <h1>{count}</h1>
+  <button onClick={() => setCount(count + 1)}>Click Me</button>
+  </>
 };
 
 export default Button;
