f931d2a61b08bfd03ec6a6165ad353de364e9d14
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-05-21 13:00:37 -0400

    removing default compenets i.e react logo

--- src/components/button.js
+++ src/components/button.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import './button.css'
 
 const Button = () => {
   const [count, setCount] = useState(0);
   return <>
   <h1>{count}</h1>
-  <button onClick={() => setCount(count + 1)}>Click Me</button>
+  <button className="button" onClick={() => setCount(count + 1)}>Click Me</button>
   </>
 };
 
