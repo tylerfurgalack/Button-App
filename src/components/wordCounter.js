ce67e031e2fc3f0bcdfba35ce9561069b0164c3a
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-05-26 17:49:26 -0400

    created arry with 31 values

---
+++ src/components/wordCounter.js
@@ -0,0 +1,39 @@
+function wordCounter() {
+  const numArry = [
+    "sıfır",
+    "bir",
+    "iki",
+    "üç",
+    "dört",
+    "beş",
+    "altı",
+    "yedi",
+    "sekiz",
+    "dokuz",
+    "on",
+    "on bir",
+    "on iki",
+    "on üç",
+    "on dört",
+    "on beş",
+    "on altı",
+    "on yedi",
+    "on sekiz",
+    "on dokuz",
+    "yirmi",
+    "yirmi bir",
+    "yirmi iki",
+    "yirmi üç",
+    "yirmi dört",
+    "yirmi beş",
+    "yirmi altı",
+    "yirmi yedi",
+    "yirmi sekiz",
+    "yirmi dokuz",
+    "otuz bir",
+  ];
+  for (let i=0; i<numArry.length; i++){
+
+  };
+  return numArry[i]
+};
