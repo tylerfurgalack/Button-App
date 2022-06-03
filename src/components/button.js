4d079fe0a45f7c5c02e7fe30f898e5208dd6c3c5
tylerfurgalack <104029862+tylerfurgalack@users.noreply.github.com>
2022-06-03 16:12:29 -0400

    fixed useEffect with double timers, turnery operator in <h1>?

--- src/components/button.js
+++ src/components/button.js
@@ -1,35 +1,37 @@
 import { useState, useEffect } from "react";
 import "./button.css";
-import WordCounter from "./wordCounter";
+import WordCounter from "./WordCounter";
 //"Boşalmak"
 const Button = () => {
-  const [text, setText] = useState("sıfır");
   const [count, setCount] = useState(0);
-  useEffect(()=> {
-    if(count === 31){
-      setTimeout(()=>{
-        setCount(0);
-      }, 5000);
+  useEffect(() => {
+    if (count === 10) {
+      setTimeout(() => {
+        setCount(11);
+        setTimeout(() => {
+          setCount(0);
+        }, 3000);
+      }, 2000);
     }
-  },[count]);
+  }, [count]);
 
   return (
     <div className="container">
       <div className="wordCountContainer">
-        <h1 className="wordCountText">{count}</h1>
+        <h1 className="wordCountText">{count > 10 ? 10 : count}</h1>
       </div>
       <div className="buttonContainer">
         <button
           className="button"
           onClick={() => setCount(count + 1)}
-          disabled={count === 31}
+          disabled={count >= 10}
         >
           Keep Clicking
         </button>
       </div>
       <div className="wordCountContainer">
         <h1 className="wordCountText">
-          <WordCounter count={count} text={text} setText={setText}/>
+          <WordCounter count={count} />
         </h1>
       </div>
     </div>
